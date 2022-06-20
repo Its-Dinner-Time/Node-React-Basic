e92c4e18561d9ad36a255c3280b27b57f0700bc6
dinner-time <dinnertime.dev@gmail.com>
2022-06-20 19:16:16 +0900

    client routing

--- Boiler-Plate/client/src/App.jsx
+++ Boiler-Plate/client/src/App.jsx
@@ -1,5 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/views/Navbar/Navbar';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Footer from './components/views/Footer/Footer';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- Boiler-Plate/client/src/components/views/Navbar/Navbar.jsx
+++ Boiler-Plate/client/src/components/views/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
-      <div>Navbar</div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/login">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/register">Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
